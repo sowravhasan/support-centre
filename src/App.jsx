6c027dd21464ed92829df40be455092d34a046b3
sowravhasan <sowrav01825611740@gmail.com>
2023-09-14 21:41:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl text-center font-bold my-4'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
